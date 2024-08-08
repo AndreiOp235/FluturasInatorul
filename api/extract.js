b6a168f2fff6aa030c3d0b8b4d0f12f050667da1
Coșa Andrei Valeriu <42703353+AndreiOp235@users.noreply.github.com>
2024-08-08 18:27:57 +0300

    ceva ?

--- api/extract.js
+++ api/extract.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
                 let ceva = entry;
               res.json({ fileName: entry.fileName ,
                 hardcoed: "gigelium",
-                tip: typeof(entry)
+                tip: Object.prototype.toString.call(ceva)
               });
               zipFile.close();
             }
